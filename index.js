d17eb183af8d26b9bec73415442915d09cafc681
dilancelik18 <83242771+dilancelik18@users.noreply.github.com>
2025-05-28 18:31:13 +0300

    Node.js proje eklendi ve test pipeline kuruldu

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require(express);
-const todos = require(./routes/todos);
+const todos = require("./routes/todos");
 
 const app = express();
 app.use(express.json());
-app.use(/todos, todos);
+app.use("/todos", todos);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log());
--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,10 @@
-const express = require(express);
+const express = require("express");
 const router = express.Router();
 
-let todoList = [{ id: 1, task: ilk iş, done: false }];
+let todoList = [{ id: 1, task: "ilk iş", done: false }];
 
-router.get(/, (req, res) => {
+router.get("/", (req, res) => {
   res.json(todoList);
 });
 
 module.exports = router;
-
